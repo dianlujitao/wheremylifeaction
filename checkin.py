ed554a846dc7624d7f814edc1270144991259898
dianlujitao <dianlujitao@gmail.com>
2021-07-04 19:49:24 +0800

    Fix request

--- checkin.py
+++ checkin.py
@@ -2,21 +2,21 @@ import requests
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
-    'Referer': 'http://wheremylife.cn/home.htm'
+    'Referer': 'https://wheremylife.cn/home'
 }
 session = requests.Session()
 
-url = 'http://wheremylife.cn/user/login'
+url = 'https://wheremylife.cn/1.0/user/login'
 
 payload = {
     'email': input(),
-           'password': input()
-    }
+    'password': input()
+}
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
-    'Referer': 'http://wheremylife.cn/home.htm'
+    'Referer': 'https://wheremylife.cn/home'
 }
 s = session.post(url, data=payload, headers=headers)
-s2 = session.post('http://wheremylife.cn/user/resetBook', headers=headers)
+s2 = session.post('https://wheremylife.cn/1.0/user/resetBook', headers=headers)
 print('打卡成功！')
